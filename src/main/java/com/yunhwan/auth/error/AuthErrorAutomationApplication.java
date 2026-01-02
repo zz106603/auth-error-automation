43f9358c908b8ae2f59dbf21fe01a217b158e2dc
YUNHWAN <629jyh7@naver.com>
2026-01-02 16:07:24 +0900

    add: 프로젝스 초기 세팅 - gitIgnore 추가 - github Issue 템플릿 추가 - github PR 템플릿 추가

---
+++ src/main/java/com/yunhwan/auth/error/AuthErrorAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.yunhwan.auth.error;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthErrorAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthErrorAutomationApplication.class, args);
+	}
+
+}
