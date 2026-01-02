fb7467cb322a91e3f757a15fa4eaec945274df4c
YUNHWAN <629jyh7@naver.com>
2026-01-02 16:07:24 +0900

    add: 프로젝스 초기 세팅 - gitIgnore 추가 - github Issue 템플릿 추가 - github PR 템플릿 추가

---
+++ src/test/java/com/yunhwan/auth/error/AuthErrorAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunhwan.auth.error;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthErrorAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
