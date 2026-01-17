2c1e65afe2c2eb599e804cd2d0cf021a28e74428
YUNHWAN <629jyh7@naver.com>
2026-01-17 14:24:54 +0900

    add: Facade 추가 - AuthError 엔티티에 record 메서드 추가 - writer에 toSave 추가 - properties 추가(sourceService, enviroment

--- src/main/java/com/yunhwan/auth/error/app/autherror/AuthErrorFacade.java
+++ src/main/java/com/yunhwan/auth/error/app/autherror/AuthErrorFacade.java
@@ -1,5 +1,28 @@
 package com.yunhwan.auth.error.app.autherror;
 
-// 외부 유스케이스 진입점
+
+import com.yunhwan.auth.error.app.api.auth.dto.AuthErrorRecordRequest;
+import com.yunhwan.auth.error.app.api.auth.dto.AuthErrorRecordResponse;
+import com.yunhwan.auth.error.domain.autherror.AuthError;
+import com.yunhwan.auth.error.usecase.autherror.AuthErrorWriter;
+import com.yunhwan.auth.error.usecase.autherror.dto.AuthErrorWriteResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthErrorFacade {
+
+    private final AuthErrorWriter authErrorWriter;
+
+    public AuthErrorRecordResponse record(AuthErrorRecordRequest req) {
+
+        AuthError authError = AuthError.builder()
+                .requestId(req.requestId())
+                .occurredAt(req.occurredAt())
+                .build();
+
+        AuthErrorWriteResult result = authErrorWriter.record(authError);
+        return new AuthErrorRecordResponse(result.authErrorId(), result.outboxId());
+    }
 }
