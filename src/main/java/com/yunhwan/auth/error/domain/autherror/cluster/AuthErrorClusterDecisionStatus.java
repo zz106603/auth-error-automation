73e8d1057e0ebb349a5e6ab290bf0bfae1de8370
YUNHWAN <629jyh7@naver.com>
2026-02-02 20:39:46 +0900

    feat: Cluster Decision, Apply 제거

--- src/main/java/com/yunhwan/auth/error/domain/autherror/cluster/AuthErrorClusterDecisionStatus.java
+++
@@ -1,7 +0,0 @@
-package com.yunhwan.auth.error.domain.autherror.cluster;
-
-public enum AuthErrorClusterDecisionStatus {
-    APPLIED,
-    PARTIAL,
-    FAILED
-}
--- src/main/java/com/yunhwan/auth/error/usecase/autherror/cluster/AuthErrorClusterDecisionApplier.java
+++
@@ -1,8 +0,0 @@
-package com.yunhwan.auth.error.usecase.autherror.cluster;
-
-import com.yunhwan.auth.error.usecase.autherror.dto.ApplyClusterDecisionCommand;
-import com.yunhwan.auth.error.usecase.autherror.dto.ApplyClusterDecisionResult;
-
-public interface AuthErrorClusterDecisionApplier {
-    ApplyClusterDecisionResult apply(ApplyClusterDecisionCommand cmd);
-}
